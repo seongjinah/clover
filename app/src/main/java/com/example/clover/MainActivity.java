5d065125c1dd9429c192c398e529688b4e661734
seongjinah <5829341@naver.com>
2020-10-15 19:16:06 +0900

    1. color 추가 Primary, PrimaryLight, PrimaryDark, Accent 2. drawable 추가 home, clover, trashcan, diarh, letter, logout 3. navigation drawer 완성 - menu, header로 디자인 - 각 화면으로 연결

--- app/src/main/java/com/example/clover/MainActivity.java
+++ app/src/main/java/com/example/clover/MainActivity.java
@@ -1,14 +1,76 @@
 package com.example.clover;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    androidx.appcompat.widget.Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*Hook*/
+        drawerLayout = findViewById(R.id.drawerlayout);
+        navigationView = findViewById(R.id.navigationview);
+        toolbar = findViewById(R.id.toolbar_main);
+
+        /*Tool Bar*/
+        setSupportActionBar(toolbar);
+
+        /*Navigation Drawer Menu*/
+        //Hide or show item
+        Menu menu = navigationView.getMenu();
+        menu.findItem(R.id.nav_logout).setVisible(false);
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigations_drawer_open, R.string.navigations_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.setCheckedItem(R.id.nav_home);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.nav_home:
+                break;
+
+            case R.id.nav_diary:
+                Intent intent = new Intent(MainActivity.this, DiaryActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.nav_wiseword:
+                Intent intent2 = new Intent(MainActivity.this, WisewordActivity.class);
+                startActivity(intent2);
+                break;
+
+            case R.id.nav_wrongthrow:
+                Intent intent3 = new Intent(MainActivity.this, WorryThrowActivity.class);
+                startActivity(intent3);
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
